321122b4edc74daadb07c07d4b6920d0685d1725
Yuji Noizumi <noizumi@websquare.co.jp>
2016-09-09 10:32:50 +0900

    AlertDialog追加 空入力時のエラー表示と0割のエラーをAlertDialogで表示するようにした。

--- app/src/main/java/com/example/noizumi/calcapp/MainActivity.java
+++ app/src/main/java/com/example/noizumi/calcapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.noizumi.calcapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import java.math.BigDecimal;
@@ -14,15 +17,34 @@ public class MainActivity extends AppCompatActivity {
     EditText editText2;
     Intent intent;
 
+    // エラーメッセージ表示用警告ダイアログ
+    private void showAlertDialog(String mes) {
+        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("エラー");
+        alertDialogBuilder.setMessage(mes);
+
+        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setPositiveButton("OK",null);
+
+        // AlertDialogを作成して表示する
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
     // EditTextの値を取得
-    protected void getValues(){
+    private boolean getValues(){
         editText1 = (EditText)findViewById(R.id.editText1);
         editText2 = (EditText)findViewById(R.id.editText2);
         intent = new Intent(this, ResultActivity.class);
 
+        if ((editText1.length()!=0) && (editText2.length()!=0)){
+            return true;
+        }else{
+            showAlertDialog("数値を入力して下さい。");
+        }
+        return false;
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +55,9 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener button_plusClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getValues();
+                if(!getValues()){
+                    return;
+                }
 
                 BigDecimal e1 = new BigDecimal(editText1.getText().toString());
                 BigDecimal e2 = new BigDecimal(editText2.getText().toString());
@@ -50,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener button_minusClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getValues();
+                if(!getValues()){
+                    return;
+                }
 
                 BigDecimal e1 = new BigDecimal(editText1.getText().toString());
                 BigDecimal e2 = new BigDecimal(editText2.getText().toString());
@@ -67,7 +93,9 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener button_mulClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getValues();
+                if(!getValues()){
+                    return;
+                }
 
                 BigDecimal e1 = new BigDecimal(editText1.getText().toString());
                 BigDecimal e2 = new BigDecimal(editText2.getText().toString());
@@ -84,7 +112,9 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener button_divClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getValues();
+                if(!getValues()){
+                    return;
+                }
 
                 BigDecimal e1 = new BigDecimal(editText1.getText().toString());
                 BigDecimal e2 = new BigDecimal(editText2.getText().toString());
@@ -92,16 +122,15 @@ public class MainActivity extends AppCompatActivity {
                 String result;
                 if(e2.doubleValue() != 0.0) {
                     result = e1.divide(e2,3, RoundingMode.HALF_UP).toString();
+                    intent.putExtra("FORMULA", e1.toString()+"÷"+e2.toString()+"＝");
+                    intent.putExtra("RESULT", result);
+                    startActivity(intent);
                 }else {
-                    result ="0で除算はできません。";
+                    showAlertDialog("0で除算はできません。");
                 }
-                intent.putExtra("FORMULA", e1.toString()+"÷"+e2.toString()+"＝");
-                intent.putExtra("RESULT", result);
-                startActivity(intent);
             }
         };
         findViewById(R.id.button_div).setOnClickListener(button_divClickListener);
 
     }
-
 }
